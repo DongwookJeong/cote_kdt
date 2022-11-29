3b55251211aa2d8d0e7fa56cd99924792555fd6c
정동욱 <nolhoon211@gmail.com>
2022-11-29 15:01:26 +0900

    텍스트 파일, 처리 방향 구상

---
+++ add.js
@@ -0,0 +1,12 @@
+// let a = header;
+// let b = main;
+// let c = footer;
+function html(head, body) {
+  // head = ""
+  let a = "header";
+  let b = "main";
+  let c = "footer";
+  body = a + b + c;
+  return body;
+}
+console.log(html());
